63c7d8f99ab90f0cb4e1cbb58dcb62b7b980732e
Jean Stthrudell <jeanc.lopes@hotmail.com>
2021-01-07 20:29:57 -0300

    first commit

---
+++ app/main.ts
@@ -0,0 +1,25 @@
+import { Discord, On, Client } from "@typeit/discord";
+
+@Discord()
+abstract class AppDiscord {
+
+  @On("message")
+  private onMessage() {
+    console.log({ arguments });
+  }
+
+}
+
+async function start() {
+  const client = new Client({
+    classes: [
+      AppDiscord
+    ],
+    silent: false,
+    variablesChar: ":"
+  });
+
+  await client.login("Nzk2ODYzMzExNzU1ODcwMjM4.X_eGyg.EAmwxPo5C235JvTAi8PhmyT5mpc");
+}
+
+start();
