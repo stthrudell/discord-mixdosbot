fe606b3da57b6a47aff5b25d0d306e2f4422b6ed
Jean Stthrudell <jeanc.lopes@hotmail.com>
2022-03-05 17:24:46 -0300

    fix some things on steaid observer

--- app/commands/SteamIdObserver.ts
+++ app/commands/SteamIdObserver.ts
@@ -11,38 +11,61 @@ export default class PingObserver implements IObserver {
 
         const messageSplited = eventMessage.message.content.replace("!", "").split(" ");
         const steamId = messageSplited.slice(1)[0];
-        
-        if(!steamId){
-            const user = await User.findOne({
+
+        const user = await User.findOne({
+			discordId: userId,
+		});
+
+		if (!steamId) {
+			const user = await User.findOne({
 				discordId: userId,
 			});
 
-            if(user) {
-                eventMessage.message.channel.send(`${userAuthor} seu steam id é ${user?.steamId}`);
-            } else {
-                eventMessage.message.channel.send(`${userAuthor} você não tem o SteaID 64 Cadastrado. Para cadastrar, digite !steamid xxxxxxxxxxxxxxxxx`);
-            }
+			if (user) {
+				eventMessage.message.channel.send(
+					`${userAuthor} seu steam id é ${user?.steamId}`
+				);
+			} else {
+				eventMessage.message.channel.send(`
+                    ${userAuthor} você não tem o SteaID 64 Cadastrado. Para cadastrar, digite !steamid xxxxxxxxxxxxxxxxx \n Para achar sua SteamID 64 basta entrar no site https://steamid.io e procurar pelo seu username!
+                `);
+			}
+		} else if (steamId == "delete") {
+			if (!user) {
+				eventMessage.message.channel.send(
+					`${userAuthor} Você não possui nenhuma SteamID cadastrada!`
+				);
+			} else {
+				await user.remove();
+				eventMessage.message.channel.send(
+					`${userAuthor} SteamID deletada com sucesso!`
+				);
+			}
+		} else if (steamId.length != 17) {
+			eventMessage.message.channel.send(
+				`${userAuthor} SteamID no formato inválido, você deve cadastrar a SteamID 64!`
+			);
+		} else {
+			const user = await User.findOne({
+				discordId: userId,
+			});
 
-        } else if(steamId.length != 17) {
-            eventMessage.message.channel.send(`${userAuthor} SteamID no formato inválido, você deve cadastrar a SteamID 64!`);
-        } else {
+			if (user) {
+				eventMessage.message.channel.send(
+					`${userAuthor} esta steam id já esta cadastrada.`
+				);
+			} else {
+				const user = await User.create({
+					discordId: userId,
+					steamId: steamId,
+				});
+				await user.save();
 
-            const user = await User.findOne({
-                discordId: userId,
-            });
-
-            if(user) {
-                eventMessage.message.channel.send(`${userAuthor} esta steam id já esta cadastrada.`);
-            } else {
-                const user = await User.create({
-                    discordId: userId,
-                    steamId: steamId
-                })
-                await user.save();
-                
-                eventMessage.message.channel.send(`${userAuthor} SteamID cadastrado com sucesso!`);
-            }            
-        }
+				eventMessage.message.channel.send(
+					`${userAuthor} SteamID cadastrado com sucesso!`
+				);
+			}
+		}
 
     }
 }
